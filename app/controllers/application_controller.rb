5511979e2b2b7d4886ad6f1220d66f6d1d9d32d7
ShinjiShiozaki <shinji.shiozaki@nifty.com>
2024-05-06 15:13:04 +0900

    ユーザーの新規登録機能完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  #240506)add
+  #before_action :authenticate_user!
 
   #binding.pry
   
@@ -9,6 +13,11 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == 'admin' && password == '2222'
     end
-  #binding.pry
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :sei_kanji, :mei_kanji, :sei_kana, :mei_kana, :tanjyoubi])
+    end
+  
+    #binding.pry
 end
 end

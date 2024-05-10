c8f1876c8eece08435dae8544347725830862a7e
ShinjiShiozaki <shinji.shiozaki@nifty.com>
2024-05-11 06:00:15 +0900

    Rubocopを再実行

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :item do
-    mei                 {'テスト商品名'}
-    setsumei            {'テスト商品の説明文'}
-    category_id         {2}
-    jyoutai_id          {2}
-    haisouryou_futan_id {2}
-    todoufuken_id       {2}
-    hassou_nissuu_id    {2}
-    kakaku              {1000}
+    mei                 { 'テスト商品名' }
+    setsumei            { 'テスト商品の説明文' }
+    category_id         { 2 }
+    jyoutai_id          { 2 }
+    haisouryou_futan_id { 2 }
+    todoufuken_id       { 2 }
+    hassou_nissuu_id    { 2 }
+    kakaku              { 1000 }
 
     association :user
 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -30,27 +30,27 @@ RSpec.describe Item, type: :model do
       it 'カテゴリーの情報が必須であること' do
         @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include "Category must be other than 1"
+        expect(@item.errors.full_messages).to include 'Category must be other than 1'
       end
       it '商品の状態の情報が必須であること' do
         @item.jyoutai_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include "Jyoutai must be other than 1"
+        expect(@item.errors.full_messages).to include 'Jyoutai must be other than 1'
       end
       it '配送料の負担の情報が必須であること' do
         @item.haisouryou_futan_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include "Haisouryou futan must be other than 1"
+        expect(@item.errors.full_messages).to include 'Haisouryou futan must be other than 1'
       end
       it '発送元の地域の情報が必須であること' do
         @item.todoufuken_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include "Todoufuken must be other than 1"
+        expect(@item.errors.full_messages).to include 'Todoufuken must be other than 1'
       end
       it '発送までの日数の情報が必須であること' do
         @item.hassou_nissuu_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include "Hassou nissuu must be other than 1"
+        expect(@item.errors.full_messages).to include 'Hassou nissuu must be other than 1'
       end
       it '価格の情報が必須であること' do
         @item.kakaku = nil
@@ -60,15 +60,15 @@ RSpec.describe Item, type: :model do
       it '価格は、￥300~￥9,999,999の間のみ保存可能であること' do
         @item.kakaku = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include "Kakaku must be greater than or equal to 300"
-        @item.kakaku = 10000000
+        expect(@item.errors.full_messages).to include 'Kakaku must be greater than or equal to 300'
+        @item.kakaku = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include "Kakaku must be less than or equal to 9999999"
+        expect(@item.errors.full_messages).to include 'Kakaku must be less than or equal to 9999999'
       end
       it '価格は半角数値のみ保存可能であること' do
         @item.kakaku = '３００'
         @item.valid?
-        expect(@item.errors.full_messages).to include "Kakaku is not a number"
+        expect(@item.errors.full_messages).to include 'Kakaku is not a number'
       end
     end
   end

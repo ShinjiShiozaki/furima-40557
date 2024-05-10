698b0b58070b87b4fa686696ee7a7a4fd2aeffd8
ShinjiShiozaki <shinji.shiozaki@nifty.com>
2024-05-11 06:37:12 +0900

    Rubocopを再々実行等

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-//240510)add
 import "item_kakaku"
 
